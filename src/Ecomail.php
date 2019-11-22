da03470bd773e15d78f6e3bf52fb6584ca1070f0
Tlapi <jan@potentus.com>
2019-11-22 20:40:15 +0100

    Check status code of response

--- src/Ecomail.php
+++ src/Ecomail.php
@@ -424,6 +424,18 @@ class Ecomail
         ));
 
         $output = curl_exec($ch);
+        
+        // Check HTTP status code
+        if (!curl_errno($ch)) {
+            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($http_code >= 200 && $http_code <= 299) {
+
+            } else {
+                return [
+                    'error' => $http_code,
+                ];
+            }
+        }
 
         curl_close($ch);
 
